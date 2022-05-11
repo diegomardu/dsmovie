c516acba869346efb066fdfac9b6b1bb3bd5fe51
Diego Martins Duarte <diego.martins.duarte@accenture.com>
2022-05-11 11:55:17 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,48 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	private ScorePk id = new ScorePk();
+	public ScorePk getId() {
+		return id;
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public void setId(ScorePk id) {
+		this.id = id;
+	}
+
+	private Double value;
+	
+
+	public Score() {
+
+	}
+
+	public Score(Double value) {
+		this.value = value;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+}
